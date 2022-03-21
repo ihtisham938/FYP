db73818e1253b207a027df06df1f0865770edefe
ihtisham938 <ihtisham938@gmail.com>
2022-03-21 15:38:56 +0500

    done

--- Models/Home/HomeIndexViewModel.cs
+++ Models/Home/HomeIndexViewModel.cs
@@ -1,22 +1,31 @@
 ﻿using OnlineShoppingStore.DAL;
 using OnlineShoppingStore.Repository;
+using PagedList;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using PagedList.Mvc;
 
 namespace OnlineShoppingStore.Models.Home
 {
     public class HomeIndexViewModel
     {
         public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
-        public IEnumerable<Tbl_Product> ListOfProducts { get; set; }
-        public HomeIndexViewModel CreateModel()
+        dbMyOnlineShoppingEntities context = new dbMyOnlineShoppingEntities();
+        public IPagedList<Tbl_Product> ListOfProducts { get; set; }
+        public HomeIndexViewModel CreateModel(string search, int pageSize,int? page)
         {
-            return new HomeIndexViewModel()
+
+            SqlParameter[] param = new SqlParameter[] {
+                   new SqlParameter("@search",search??(object)DBNull.Value)
+                   };
+            IPagedList<Tbl_Product> data = context.Database.SqlQuery<Tbl_Product>("GetBySearch @search", param).ToList().ToPagedList(page?? 1, pageSize);
+            return new HomeIndexViewModel
             {
-                ListOfProducts = _unitOfWork.GetRepositoryInstance<Tbl_Product>().GetAllRecords()
-        };
+                ListOfProducts = data
+            };
         }
     }
 }
